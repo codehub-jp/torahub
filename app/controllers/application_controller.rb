5d975604eae5473661615ea8449e88d5dcbc4c22
codehub-jp <codehub3110@gmail.com>
2023-04-28 10:06:20 +0900

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
     private
 
-        def require_user_logged_in
-            unless logged_in?
-                redirect_to login_url
-            end
+    def require_user_logged_in
+        unless logged_in?
+            redirect_to login_url
         end
+    end
 
-        def counts(user)
-            @count_boards = user.boards.count
-        end
+    def counts(user)
+        @count_boards = user.boards.count
+    end
 end
